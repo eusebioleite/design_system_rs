5d0601a7df616ed6896c1e165b55ef710cafd46c
Eusébio Mackenzie Leite <eusebio.leite16@gmail.com>
2026-03-24 10:13:07 -0300

    Separated ui logic between two objects, toolbar.rs and content.rs

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,21 +1,15 @@
 pub mod styles;
 pub mod toolbar;
+pub mod content;
 
-use iced::widget::{column, container, Space};
-use iced::{Element, Length};
+use iced::widget::{column, Space};
+use iced::{Element};
 
 use crate::core::message::Message;
 use crate::core::state::State;
 
 pub fn view(_state: &State) -> Element<'_, Message> {
     let toolbar = toolbar::render();
-
-    // CONTENT PRINCIPAL
-    let content = container(Space::new())
-        .width(Length::Fill)
-        .height(Length::Fill)
-        .center_x(Length::Fill);
-
-    // OUTPUT: TOOLBAR + CONTENT
+    let content = content::render();
     column![toolbar, Space::new().height(10), content].into()
 }
